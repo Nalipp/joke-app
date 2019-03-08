3b0ccb403e9227caab94a30b6b620759e9ad1654
Nalipp <nateplipp@gmail.com>
2019-03-08 11:30:44 -0800

    fix mongodb url bug

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 mongoose.set('debug', true);
 var Joke = require("./joke");
 
-if (process.env.MONGO_URI) {
-  console.log('connecting to MONGO_URI..');
-  mongoose.connect(process.env.MONGO_URI);
+if (process.env.MONGO_URIDB) {
+  console.log('connecting to MONGODB_URI..');
+  mongoose.connect(process.env.MONGODB_URI);
 } else {
   console.log('connecting to localhost/joke-app...');
   mongoose.connect('mongodb://localhost/joke-app');
