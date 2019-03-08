fba23ad94a8d96f65d11ad3d4300e74307fcceb7
Nalipp <nateplipp@gmail.com>
2019-03-08 11:50:30 -0800

    heroku troubleshooting

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,14 +2,9 @@ var mongoose = require('mongoose');
 mongoose.set('debug', true);
 var Joke = require("./joke");
 
-console.log('general...', process.env);
-
 if (process.env.MONGO_URI) {
   console.log('connecting to MONGO_URI..');
   mongoose.connect(process.env.MONGO_URI);
-} else if (process.env.MONGODB_URI) {
-  console.log('connecting to MONGODB_URI..');
-  mongoose.connect(process.env.MONGODB_URI);
 } else {
   console.log('connecting to localhost/joke-app...');
   mongoose.connect('mongodb://localhost/joke-app');
